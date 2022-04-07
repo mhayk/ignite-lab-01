04a18d42a34eef0ea116a399b819811a65390a44
Mhayk Whandson <hi@mhayk.com>
2022-04-07 20:48:46 +0100

    product service to create a new product using graphQL has been created
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -1,7 +1,8 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 import { ProductsService } from 'src/services/products.service';
+import { CreateProductInput } from '../inputs/create-product-input';
 import { Product } from '../models/products';
 
 @Resolver('test')
@@ -12,5 +13,11 @@ export class ProductsResolver {
   // @UseGuards(AuthorizationGuard)
   products() {
     return this.productsService.listAllProducts();
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Product)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
   }
 }
